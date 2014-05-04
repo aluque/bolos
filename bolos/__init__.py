850ac358cb3160e82deab97c45bdbd2f48a497e0
Alejandro Luque <aluque@iaa.es>
2014-05-04 19:23:04 +0200

    Reorganization for packaginf with setuptools.

---
+++ bolos/__init__.py
@@ -0,0 +1,2 @@
+# Usually these are the only modules that are useful to the user.
+__all__ = ['solver', 'grid']
---
+++ setup.py
@@ -0,0 +1,67 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bolos",
+    version="0.1",
+    description="A library to solve the Boltzmann equation",
+    long_description="",
+
+    # The project URL.
+    url='https://github.com/aluque/bolos',
+
+    # Author details
+    author='Alejandro Luque',
+    author_email='aluque@iaa.es',
+
+    # Choose your license
+    license='LGPLv2',
+
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)'
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='physics plasma boltzmann',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages.
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed.
+    install_requires = ['numpy', 'scipy'],
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={},
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages.
+    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={},
+)
