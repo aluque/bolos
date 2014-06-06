07e36fd344a0cfca8766f7f3bd9e6e30de02cb25
Alejandro Luque <aluque@iaa.es>
2014-06-06 19:43:56 +0200

    Documentation extended in grid.py

--- bolos/grid.py
+++ bolos/grid.py
@@ -1,10 +1,38 @@
 """ Routines to handle different kinds of grids (linear, quadratic, logarithmic)
 """
 import numpy as np
+from scipy.interpolate import interp1d
 
 class Grid(object):
-    """ Grid class.  Different kinds of grid are defined by a function
-    f and its inverse; these define the spacing within the grid.  """
+    """ Class to define energy grids.
+
+    This class encapsulates the information about an energy grid.
+
+    Parameters
+    ----------
+    x0 : float
+       Lowest boundary energy.
+    x1 : float
+       Highest energy boundary.
+    n : float
+       Number of cells
+
+    Notes
+    -----
+    This is a base class and you usually do not want to instantiate it
+    directly.  You can define new grid classes by subclassing this class and
+    then defining an `f` method that maps energy to a new variable `y`
+    that is divided uniformly.
+
+    See Also
+    --------
+    LinearGrid : A grid with linear spacings (constant cell length).
+    QuadraticGrid : A grid with quadratic spacings (linearly increasing 
+       cell length).
+    GeometricGrid : A grid with geometrically increasing cell lengths.
+    LogGrid : A logarithmic grid.
+
+    """
     def __init__(self, x0, x1, n):
         self.x0 = x0
         self.x1 = x1
@@ -28,8 +56,53 @@ class Grid(object):
         # This is the spacing of the mapped x
         self.df = fx[1] - fx[0]
         
+        # This is useful in some routines that integrate eps**1/2 * f
+        self.d32 = self.b[1:]**1.5 - self.b[:-1]**1.5
+
+        self._interp = None
+
+
+    def interpolate(self, f, other):
+        """ Interpolates into this grid an eedf defined in another grid. 
+
+        Parameters
+        ----------
+        f : array or array-like
+           The original EEDF
+        other : :class:`Grid`
+           The old grid, where `f` is defined.
+
+        Returns
+        -------
+        fnew : array or array-like
+           An EEDF defined in our grid.
+
+
+        """
+        if self._interp is None:
+            # Sould we extrapolate linearly instead od by closest value?
+            self._interp = interp1d(np.r_[other.x0, other.c, other.x1],
+                                    np.r_[f[0], f, f[-1]],
+                                    bounds_error=False, fill_value=0)
+        
+        return self._interp(self.c)
+
+
+
     def cell(self, x):
-        """ Returns the cell index containing the value x. """
+        """ Returns the cell index containing the value x. 
+
+        Parameters
+        ----------
+        x : float
+           The value x which you want to localize.
+
+        Returns
+        -------
+        index : int
+           The index to the cell containing x
+
+        """
         return int((self.f(x) - self.fx0) / self.df)
         
 
@@ -45,7 +118,7 @@ class LinearGrid(Grid):
 class QuadraticGrid(Grid):
     """ A grid with quadratic spacing. """
     def f(self, x):
-        return sqrt(x - self.x0)
+        return np.sqrt(x - self.x0)
 
     def finv(self, w):
         return w**2 + self.x0
@@ -54,8 +127,7 @@ class QuadraticGrid(Grid):
 class GeometricGrid(Grid):
     """ A grid with geometrically progressing spacing. To be more precise, 
     here the length
-    of cell i+1 is r times the length of cell i.  Perhaps calling this a
-    'geometric' grid would be more appropriate.
+    of cell i+1 is r times the length of cell i.
     """
     def __init__(self, x0, x1, n, r=1.1):
         self.r = r
@@ -90,6 +162,36 @@ class LogGrid(Grid):
 
     def finv(self, w):
         return np.exp(w) - self.s + self.x0
+
+
+class AutomaticGrid(Grid):
+    """ A grid set automatically using a previous estimation of the EEDF
+    to fix a peak energy.  """
+    def __init__(self, grid, f0, delta=1e-4):
+        # We will create a new grid where the number of particles is roughly
+        # the same inside each cell and the number of cells is the same as in
+        # grid.
+        
+        # TODO: This is also calculated in the solver.BoltzmannSolver class
+        cum = np.r_[0.0, np.cumsum(grid.d32 * f0)]
+
+
+        # If we had integrated f0 exactly, cum[-1] would be 1.  However it may
+        # be slightly differentso we will renormalize here to prevent an error
+        # when we interpolate for a number very close to 1.0.
+        cum[:] = cum / cum[-1]
+        
+        interp = interp1d(cum, grid.b)
+        nnew = np.linspace(0.0, 1.0, grid.n + 1)
+
+        self.n, self.x0, self.x1 = grid.n, grid.x0, grid.x1
+
+        self.b = interp(nnew)
+
+        self.c = 0.5 * (self.b[1:] + self.b[:-1])
+        self.d = np.diff(self.b)
+
+        self._interp = None
 
 
 
