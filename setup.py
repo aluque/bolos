3c23c5665d61b704f0be5349cf8e87ef33392b4d
Alejandro Luque <aluque@iaa.es>
2014-06-10 16:21:30 +0200

    Bug corrected in setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         'Topic :: Scientific/Engineering :: Atmospheric Science',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)'
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
