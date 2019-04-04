75fb991c437ee966cf06f173f46a3bc2afd2719d
bangshiuh <bangshiuh.chen@gmail.com>
2019-04-04 22:05:23 +0800

    add cs_converter

---
+++ samples/cs_converter.py
@@ -0,0 +1,26 @@
+import numpy as np
+import scipy.constants as co
+from bolos import parser, solver, grid
+try:
+    import ruamel_yaml as yaml
+except ImportError:
+    from ruamel import yaml
+
+
+def main():
+
+    # Parse the cross-section file in BOSIG+ format and load it into the
+    # solver.
+    with open("Cross section.txt") as fp:
+        processes = parser.parse(fp)
+
+    data = dict()
+
+    data['electron'] = "MaxwellBoltzmann"
+    data['cross_section'] = processes
+
+    outfile = open("lxcat.yaml", "w")
+    yaml.dump(data, outfile)
+
+if __name__ == '__main__':
+    main()
